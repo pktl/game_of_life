094ab5f53a4d53773388ca470177fce366e838e7
pktl <peter@bckrnet.de>
2018-10-10 10:25:53 +0200

    Update doc strings

--- life.py
+++ life.py
@@ -1,17 +1,31 @@
-'''
-Created on 30.09.2018
-
-@author: PB
-'''
-
 import logging
 import random
 
 class Cell:
+    """Represents one cell in the grid.
+
+    A cell has the following (public) properties:
+
+    x, y        - Position (positive int).
+    state       - Boolean: Alive (True) or Dead (False).
+    neighbors   - Dict of neighboring Cell objects. Accessed by e.g. 'N', 'SE', etc.
+
+    switch_state()                  - Switch from Alive to Dead and vice versa.
+    set_neighbor(direction, cell)   - Set Cell object as neighbor.
+    count_live_neighbors()          - Returns number of neighbors that are alive.
+    set_new_state(new_state)        - Set new state for the next iteration.
+    apply_new_state()               - Set state = new_state.
+    """
     def __init__(self, x=0, y=0, state=False):
+        """Init Cell object.
+
+        x, y    -- Position (positive int, defaults 0).
+        state   -- Initial state (default False).
+        """
         self.x = x
         self.y = y
         self.state = state
+        # Initially, a cell has no neighbors.
         self.neighbors = {
                 "N":  None,
                 "NW": None,
@@ -29,35 +43,48 @@ class Cell:
         return("x={}, y={}, state={}".format(self.x, self.y, self.state))
 
     def switch_state(self):
+        """Switch alive/dead state."""
         self.state = not self.state
         self.logger.debug("Cell {} switched state to {}".format(id(self), self.state))
 
     def set_state(self, state):
+        """Set alive/dead state."""
         self.state = state
         self.logger.debug("Cell {} state set to {}".format(id(self), self.state))
 
     def set_neighbor(self, direction, cell):
         """Set a neighbor in position relative to this cell.
-        Direction is either N, NW, W, SW, S, SE, E, NE.
+        
+        direction   -- N, NW, W, SW, S, SE, E or NE.
+        cell        -- Neighboring Cell object.
         """
         self.neighbors[direction] = cell
         self.logger.debug("[{}, {}] located {} of [{}, {}]".format(cell.x, cell.y, direction, self.x, self.y))
 
     def count_live_neighbors(self):
+        """Return neighboring cells that are alive."""
         n_live_neighbors = sum(1 for x in self.neighbors.values() if x is not None and x.state)
         self.logger.debug("[{}, {}] has {} neighbors".format(self.x, self.y, n_live_neighbors))
         return(n_live_neighbors)
 
     def set_new_state(self, new_state):
+        """Set new state for the next iteration."""
         self.new_state = new_state
         self.logger.debug("[{}, {}] new state {}".format(self.x, self.y, self.new_state))
 
     def apply_new_state(self):
+        """Apply new state."""
         self.state = self.new_state
         self.logger.debug("[{}, {}] new state {} applied".format(self.x, self.y, self.state))
 
 
 class Life:
+    """Represents the grid of Cells and implements game logic.
+    
+    After creating the Life object, the next iteration of the game is generated using the next_generation() method.
+    """
+
+    # This translates compass points to x,y offsets.
     directions = {
         "N" : [ 0, +1],
         "NW": [-1, +1],
@@ -70,32 +97,41 @@ class Life:
         }
     
     def __init__(self, width=5, height=5):
+        """Initialize the grid, populate it with Cell objects and announce neighboring cells.
+
+        width   -- Number of cells in x direction (default 5)
+        height  -- Number of cells in y direction (default 5)
+        """
         self.width = width
         self.height = height
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.debug("Life {} initialized: {}".format(id(self), self))
         self.create_cells()
         self.randomize_cells()
-        self.invert_cells()
+        #self.invert_cells()
         self.set_neighbors()
 
     def __repr__(self):
         return("width={}, height={}".format(self.width, self.height))
 
     def create_cells(self):
+        """Create Cell objects for each grid element."""
         self.cells = [ [Cell(x, y, False) for y in range(self.height)] for x in range(self.width)]
         self.logger.debug(self.cells)
         self.logger.debug("All cells created")
         
     def randomize_cells(self):
+        """Randomize state of all cells."""
         [ [self.cells[x][y].set_state(bool(random.randint(0, 1))) for x in range(self.width)] for y in range(self.height)]
         self.logger.debug("All cells randomized")
 
     def invert_cells(self):
+        """Switch state of all cells."""
         [ [self.cells[x][y].switch_state() for x in range(self.width)] for y in range(self.height)]
         self.logger.debug("All cells inverted")
 
     def set_neighbors(self):
+        """Populate each cells dict of neighbors."""
         for y in range(self.height):
             for x in range(self.width):
                 for direction, dxy in self.directions.items():
@@ -103,6 +139,7 @@ class Life:
                     dy = dxy[1]
                     try:
                         if((x+dx)<0 or (y+dy)<0):
+                            # This prevents a wrapping grid by disallowing negative indices.
                             raise IndexError("Negative index not allowed!")
                         else:
                             self.cells[x+dx][y+dy]
@@ -112,6 +149,11 @@ class Life:
                         pass
 
     def next_generation(self):
+        """Calculate the next iteration using Conway's rules.
+
+        https://en.wikipedia.org/wiki/Conway's_Game_of_Life#Rules
+        """
+        # Calculate the new state of each cell, but don't apply it yet for synchronous updates.
         for y in range(self.height):
             for x in range(self.width):
                 n_live_neighbors = self.cells[x][y].count_live_neighbors()
@@ -127,31 +169,7 @@ class Life:
                         self.cells[x][y].set_new_state(True)
                     else:
                         self.cells[x][y].set_new_state(self.cells[x][y].state)
+        # Apply new state.
         for y in range(self.height):
             for x in range(self.width):
                 self.cells[x][y].apply_new_state()
-
-        
-# Not sure how to use this yet...
-def mylist(list):
-    """Custom list which disallows negative indices.
-
-    Source: https://stackoverflow.com/questions/30649912/how-to-make-python-refuse-negative-index-values
-    """
-    def __getitem__(self, n):
-        if n < 0:
-            raise IndexError("Negative index not allowed!")
-        return(list.__getitem__(self, n))
-
-
-def test():
-    game = Life(width=2, height=2)
-    
-
-if __name__=="__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    #c1 = Cell()
-    #c2 = Cell()
-    #c1.set_neighbor("N", c2)
-
-    game = Life(width=2, height=2)
